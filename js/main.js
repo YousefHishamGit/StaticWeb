cec5ef893d03904d7f1b4d494bcb296505edb0bc
YousefHishamGit <youseefhisham9@gmail.com>
2025-04-24 18:15:32 +0200

    main

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let navIcon = document.getElementById("navbar-icon");
+let navList = document.getElementById("nav-list");
+
+navIcon.addEventListener("click", function() {
+    navList.classList.toggle("active");
+
+})
